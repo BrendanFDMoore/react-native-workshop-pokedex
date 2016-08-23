768e61daf5ed8cc000e9f3097ec14045f1517478
Brendan Moore <brendan@bfdmoore.com>
2016-08-23 07:10:37 -0400

    Convert to ListView for scrolling efficiency this is a big messy commit that I don't understand completely, but it works.

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
 } from 'react-native';
 import Immutable, { List } from 'immutable';
 import { connect } from 'react-redux';
@@ -14,9 +15,59 @@ import SearchBar from '../components/SearchBar';
 import { getPokemonImageUrl } from '../utils/pokemon';
 
 class Pokedex extends Component {
+
+  constructor(props) {
+    super(props);
+    let dataSource = new ListView.DataSource({
+      sectionHeaderHasChanged: this._sectionHeaderHasChanged,
+      rowHasChanged: this._rowHasChanged,
+    });
+
+    this.state = {
+      dataSource: dataSource
+    }
+  }
+
+  componentDidMount() {
+    // let listViewScrollView = this.refs.listView.getScrollResponder();
+    // listViewScrollView.scrollTo(1); // Hack to get ListView to render fully
+  }
   
-  render() {
-    const pokeMO = (p, index) => (
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.pokemon !== this.props.pokemon) {
+      let {data, sectionIds} = this._getListViewData(nextProps.pokemon);
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(data, sectionIds)
+      })
+    }
+  } 
+  
+  _getListViewData(pokemon) {
+    let data = {};
+    let sectionIds = [];
+    
+    pokemon.map((p) => {
+      let section = 'all'; //ignore sections for now
+      if (sectionIds.indexOf(section) === -1) {
+        sectionIds.push(section);
+        data[section] = [];
+      }
+      data[section].push(p);
+    });
+
+    return {data, sectionIds};
+  }
+  
+  _sectionHeaderHasChanged(oldSection, newSection) {
+    return oldSection !== newSection;
+  }
+
+  _rowHasChanged(oldRow, newRow) {
+    return oldRow !== newRow;
+  }
+
+  pokeMO(p, index) {
+    return (
       <MediaObject 
         key={index} 
         rowId={index} 
@@ -24,26 +75,59 @@ class Pokedex extends Component {
         imgUrl={getPokemonImageUrl(p)}>
       </MediaObject>
     );
+  }
 
-    const pokeMediaObjects = (pokemonData) => {
-        console.log('pokemonData:', pokemonData);
-        if (pokemonData.length <1 )
-          return (<MediaObject name="No results to display"></MediaObject>)
-        let pokeMOs = pokemonData.map(pokeMO);
-        
-        console.log('pokeMOs:', pokeMOs);
-        
-        return (
-            pokeMOs
-        );
-    };
-    
+  _renderRow(rowData, sectionID, rowID) {
+    console.log(rowData.name, rowID);
+    return (
+      this.pokeMO(rowData, rowID)
+    );
+  }
+
+  _renderSectionHeader(data, sectionId) {
+    // var text;
+    return ( ''
+      // <View style={styles.sectionHeader}>
+      //   <Text style={styles.sectionHeaderText}>{sectionId}</Text>
+      // </View>
+    );
+  }
+
+  _renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
+    // var style = styles.rowSeparator;
+    // if (adjacentRowHighlighted) {
+    //     style = [style, styles.rowSeparatorHide];
+    // }
+    return (
+      <View key={"SEP_" + sectionID + "_" + rowID}  style={styles.separator}/>
+    );
+  }
+
+
+  // pokeMediaObjects(pokemonData) {
+  //     console.log('pokemonData:', pokemonData);
+  //     if (pokemonData.length <1 )
+  //       return (<MediaObject name="No results to display"></MediaObject>)
+  //     let pokeMOs = pokemonData.map(this.pokeMO);
+      
+  //     console.log('pokeMOs:', pokeMOs);
+      
+  //     return (
+  //         pokeMOs
+  //     );
+  // };
+  render() {
+    // {pokeMediaObjects(this.props.pokemon)}
     return (
       <View style={styles.container}>
         <Header title="POKEDEX!">
         </Header>
         <View style={styles.body}>
-            {pokeMediaObjects(this.props.pokemon)}
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData, sectionID, rowID, highlightRow) => (this._renderRow(rowData, sectionID, rowID))}
+          />
+            
         </View>
         <View style={styles.footer}>
             <SearchBar placeholder="Search..."
@@ -62,6 +146,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
 
+  },
+  separator: {
+    height: 5,
+    alignSelf: 'stretch',
   },
   body: {
     flex: 1,
